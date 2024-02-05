0d705589c08e4afae77f7e64a0695aea702e556d
JuniorDilmurodov9979~ <dilmurodovjr.com>
2024-02-05 20:26:33 +0500

    fixedd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Header } from "./componets/Header/Header";
-// import { Main } from "./componets/main/main";
-import { Search } from "./componets/search/search";
-import { List } from "./componets/list/list";
+import { List } from "./componets/List/List";
 import "./index.css";
-import { Main } from "./componets/main/main";
+import { Main } from "./componets/Main/Main";
 import "./main.css";
 export default function App() {
   return (
